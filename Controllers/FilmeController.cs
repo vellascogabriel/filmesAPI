cadf54da01d9bdced3221b31a59394f898dee48d
Gabriel Vellasco <gabriel.vellasco@transfero.com>
2022-03-03 11:23:44 -0300

    initial commit

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        public void AdicionaFilme(Filme filme)
+        {
+            filmes.Add(filme);
+        }
+    }
+}
