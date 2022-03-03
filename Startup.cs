eb705be6c35335b382a2044200322b0a62bb4334
Gabriel Vellasco <gabriel.vellasco@transfero.com>
2022-03-03 17:20:12 -0300

    finalize course Web API

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using FilmesAPI.Data;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace FilmesAPI
 {
@@ -27,6 +29,7 @@ namespace FilmesAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmesAPI", Version = "v1" });
             });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
